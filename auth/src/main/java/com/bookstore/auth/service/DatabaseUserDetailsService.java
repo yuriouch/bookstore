79b32c7e2085b9c3e87f6124e946fd9d4630ba27
Iurii <yurilap4@gmail.com>
2023-03-19 16:52:15 +0300

    UserDetailsService implementation

---
+++ auth/src/main/java/com/bookstore/auth/service/DatabaseUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.bookstore.auth.service;
+
+import com.bookstore.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("User with username '%s' was not found.", username)));
+    }
+}
